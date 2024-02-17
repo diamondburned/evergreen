7ac7556138ebb9bd56aa42badc4d31541133c54a
ezhang6811 <eric.zhang6811@gmail.com>
2024-02-17 15:28:47 -0800

    changed settings page

--- frontend/routes/settings/+page.svelte
+++ frontend/routes/settings/+page.svelte
@@ -2,18 +2,55 @@
   import { onMount } from 'svelte';
 	import NavBar from '$lib/components/NavBar.svelte';
 	import WhitePage from '$lib/components/WhitePage.svelte';
-  import { getSelf } from '$lib/api';
+  import { getAsset, uploadAsset, getSelf, updateSelf } from '$lib/api';
 
-	let avatar_img = "/stock-avatar.jpeg";
-  let avatar, fileinput;
-  let display_name = "Eric Zhang";
-  let email="ericzhang11@g.ucla.edu";
+  let fileinput;
+  let user, display_name, email, avatar_hash;
+  let display_name_changed = false;
+  let show_email_input = false;
+  let show_password_input = false;
+
+  async function setAvatar() {
+    avatar_file = await uploadAsset();
+    avatar_hash = avatar_file.hash;
+    self.update();
+  }
+
+  function changeDisplayName() {
+    display_name_changed = true;
+  }
+
+  function setDisplayName() {
+    display_name_changed = false;
+  }
+
+  function toggleEmailInput() {
+    show_email_input = ! show_email_input;
+  }
+
+  function togglePasswordInput() {
+    show_password_input = ! show_password_input;
+  }
 
   onMount(async () => {
-    let user = await getSelf();
-    console.log("hi");
-    console.log(user);
-  })
+    user = await getSelf();
+    display_name = user.display_name;
+    email = user.email;
+    avatar_hash = user.avatar_hash;
+    console.log(email, avatar_hash);
+  });
+
+  async function update() {
+    let userUpdateObj = {
+      display_name: display_name,
+      email: email,
+      avatar_hash: avatar_hash,
+      password: password,
+    };
+    await updateSelf(userUpdateObj);
+    user = await getSelf();
+    console.log(user.display_name, user.email, user.password);
+  }
   
 </script>
 
@@ -26,41 +63,71 @@
         <NavBar />
     </header>
 
-    <section class="container">
+    <form class="container" on:submit={update}>
       <h2>Settings</h2>
 
       <section class="settings">
         <div>
           <h3> Avatar </h3>
           <div class="user">
-            <img src={avatar_img} class="avatar" alt="avatar"/>
-            <input bind:this={avatar} type="file"/>
+            <img src="/stock-avatar.jpeg" class="avatar" alt="avatar"/>
+            <input bind:this={avatar_hash} type="file" accept=".jpg, jpeg, .png, .webp"/>
           </div>
         </div>
         <div>
-          <label>
-            <h3>Display Name</h3>
-            <input>
-          </label>
+          <h3>Display Name</h3>
+          <div class="user">
+            <input bind:value={display_name} on:input={changeDisplayName}>
+            {#if display_name_changed}
+            <button class="hover" type="submit" on:click={setDisplayName}>
+              Save
+            </button>
+            {/if}
+          </div>
         </div>
         <div>
           <h3> Email: </h3>
           <div class="user">
-            <p>{email}</p>
-            <button class="hover">Change email</button>
+            {#if show_email_input}
+               <input bind:value={email}>
+            {:else}
+              <p>{email}</p>
+            {/if}
+            <button class="hover" type="submit" on:click={toggleEmailInput}>
+              {#if show_email_input}
+                Save
+              {:else}
+                Change email
+              {/if}
+            </button>
           </div>
         </div>
         <div>
           <h3> Password: </h3>
           <div class="user">
-            <p>********</p>
-            <button class="hover">Change password</button>
+            {#if show_password_input}
+              <div class="change-password">
+                <p>Old Password</p>
+                <input>
+                <p>New Password</p>
+                <input>
+                <p>Confirm New Password</p>
+                <input>
+              </div>
+            {:else}
+              <p>********</p>
+            {/if}
+            <button class="hover" type="submit" on:click={togglePasswordInput}>
+              {#if show_password_input}
+                Save
+              {:else}
+                Change password
+              {/if}
+            </button>
           </div>
         </div>
       </section>
-
-      <button class="hover">Save</button>
-    </section>
+    </form>
 </WhitePage>
 
 <style lang="scss">
@@ -69,11 +136,11 @@
       flex-direction: column;
       align-items: flex-start;
     }
-    section {
+    form {
       display: flex;
       flex-direction: column;
     }
-    section.container {
+    form.container {
       align-items: baseline;
     }
     section.settings {
@@ -87,7 +154,7 @@
       }
       
       div {
-        margin-bottom: 15px;
+        margin-bottom: 36px;
       }
       div.user {
         display: flex;
@@ -118,5 +185,12 @@
 
       background-color: var(--primary);
       color: white;
+    }
+    div.change-password {
+      display: flex;
+      flex-direction: column;
+      * {
+        margin-bottom: 0.25em;
+      }
     }
 </style>
