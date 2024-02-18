0267d04b4d285097682fe43c6c1d259769fd59d2
diamondburned <diamond@arikawa-hi.me>
2024-02-17 22:43:50 -0800

    Add smoother /play iframe loading

--- frontend/routes/play/+page.svelte
+++ frontend/routes/play/+page.svelte
@@ -1,10 +1,20 @@
 <script>
+  import LoadingDots from "$lib/components/LoadingDots.svelte";
   import WhitePage from "$lib/components/WhitePage.svelte";
+  import { fade } from "svelte/transition";
+  import { cubicOut } from "svelte/easing";
+
+  let loading = true;
 </script>
 
 <WhitePage>
-  <main class="game-container">
-    <iframe src="/dashboard" title="Codingame" />
+  <main class="game-container" class:loading>
+    {#if loading}
+      <div class="loading-box" out:fade={{ easing: cubicOut, duration: 150 }}>
+        <LoadingDots padded />
+      </div>
+    {/if}
+    <iframe src="/dashboard" title="Unity game" on:load={() => (loading = false)} />
   </main>
 </WhitePage>
 
@@ -16,6 +26,17 @@
     overflow: hidden;
     background-color: var(--background);
 
+    position: relative;
+
+    .loading-box {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
     iframe {
       width: 100%;
       max-height: 100%;
@@ -24,6 +45,13 @@
 
       position: relative;
       box-shadow: 0 0 10px 0 var(--background) inset;
+
+      opacity: 1;
+      transition: opacity 0.2s ease-in-out;
+    }
+
+    &.loading iframe {
+      opacity: 0;
     }
   }
 </style>
