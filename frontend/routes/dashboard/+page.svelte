d6edf14a1b41ff0a5df753f504defb58e8c518d0
diamondburned <diamond@arikawa-hi.me>
2024-02-17 21:16:23 -0800

    Tiny style tweak

--- frontend/routes/dashboard/+page.svelte
+++ frontend/routes/dashboard/+page.svelte
@@ -235,7 +235,7 @@
   .stats-cards {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    grid-gap: 1rem;
+    grid-gap: 1.5rem;
     width: 100%;
 
     article {
